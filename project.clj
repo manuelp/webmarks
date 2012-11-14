cf9f7dcc5b534ebdc1732a7884ade17b5d758701
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-11-14 23:05:00 +0100

    v0.2.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject webmarks "0.2.0-SNAPSHOT"
+(defproject webmarks "0.2.0"
   :description "Manage bookmarks with tags."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
